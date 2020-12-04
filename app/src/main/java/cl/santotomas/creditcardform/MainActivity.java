01afbc0cd57f467146abf7b8dc74d6fa39b1f653
salva <salvador._ignacio@hotmail.com>
2020-12-03 23:53:17 -0300

    primera prueba

--- app/src/main/java/cl/santotomas/creditcardform/MainActivity.java
+++ app/src/main/java/cl/santotomas/creditcardform/MainActivity.java
@@ -3,6 +3,8 @@ package cl.santotomas.creditcardform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "estamos onCreate");
+        Toast.makeText(this, "hola mundo", Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity", "estamos funcioando Onstart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("MainActivity", "estamos en la funcion onResumen");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity", "estamos en onpause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("MainActivity", "estamos en Stop");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("MainActivity", "estamos en onRestart");
+    }
+
 }
---
+++ app/src/main/java/cl/santotomas/creditcardform/nuevoMainActivity2.java
@@ -0,0 +1,17 @@
+package cl.santotomas.creditcardform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class nuevoMainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_main2);
+        Log.i("MainActivity", "estamos funcionando");
+    }
+
+
+}
